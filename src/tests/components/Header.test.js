9bd18382e581bebd281b91bd26597e549029669d
Ins4h <Insayy@gmail.com>
2021-02-19 20:33:52 +0100

    setup production webpack build

--- src/tests/components/Header.test.js
+++ src/tests/components/Header.test.js
@@ -1,14 +1,8 @@
 import React from "react";
-import ReactShallowRenderer from "react-test-renderer/shallow";
 import { shallow } from "enzyme";
-// import toJSON from "enzyme-to-json";
 import Header from "../../components/Header";
 
 test("should render Header correctly", () => {
   const wrapper = shallow(<Header />);
   expect(wrapper).toMatchSnapshot();
-
-  // const renderer = new ReactShallowRenderer();
-  // renderer.render(<Header />);
-  // expect(renderer.getRenderOutput()).toMatchSnapshot();
 });
