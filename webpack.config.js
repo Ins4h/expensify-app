125b358191513532305e52fc190b332e61796c4c
Ins4h <Insayy@gmail.com>
2021-02-23 12:53:28 +0100

    fix firebase error

--- webpack.config.js
+++ webpack.config.js
@@ -1,13 +1,12 @@
 const path = require("path");
 const webpack = require("webpack");
 const MiniCssExtractPlugin = require("mini-css-extract-plugin");
-const { webpack } = require("webpack");
 
 process.env.NODE_ENV = process.env.NODE_ENV || "development";
 if (process.env.NODE_ENV === "test")
-  require("dotenv").config({ path: ".emv.test" });
+  require("dotenv").config({ path: ".env.test" });
 else if (process.env.NODE_ENV === "development")
-  require("dotenv").config({ path: ".emv.development" });
+  require("dotenv").config({ path: ".env.development" });
 
 module.exports = (env) => {
   const isProduction = env.production;
